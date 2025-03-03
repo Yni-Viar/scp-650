a2f5087ce860496d7cd252764e6f6d9b7e5936fa
Yni-Viar <81305791+Yni-Viar@users.noreply.github.com>
2025-03-03 18:31:39 +0300

    Upgrade to Godot 4.4
    Also:
    - Added new PlayerScript from Internal Eternal

--- src/FPSController/PlayerScript.gd
+++ src/FPSController/PlayerScript.gd
@@ -1,87 +1,83 @@
 extends CharacterBody3D
-## Created by dzejpi. License - The Unlicense. 
-## Some parts used from elmarcoh (this script is also public domain).
-## Ported from C# in 04.2024
+## Made by Yni, licensed under CC0
 class_name PlayerScript
 
+const SPEED = 4.5
+const JUMP_VELOCITY = 4
 
-@onready var player_head = $PlayerHead
+## Max health
+@export var health: Array[float] = [100]
+## Current health
+@export var current_health: Array[float] = [100]
+## Sprint toggle
+@export var sprint_enabled: bool = false
+## Move sounds toggle
+@export var move_sounds_enabled: bool = false
+## Walk sounds
+@export var footstep_sounds: Array[String]
+## Sprint sounds
+@export var sprint_sounds: Array[String]
+## Inventory toggle
+@export var enable_inventory: bool = false
+## Movement toggle (camera can be still moved through, even if this property is disabled)
+@export var can_move: bool = true
+## Current item in hand
+@export var using_item: String = ""
+## Keycards. Unlike SCP games, these keycards can open only certain doors, not other.
+## There is no leveling system.
+@export var keycards: Array[int]
+
 @onready var ray = $PlayerHead/PlayerRecoil/RayCast3D
-#@onready var watch_ray = $PlayerHead/PlayerRecoil/VisionRadius
 @onready var walk_sounds = $WalkSounds
 @onready var interact_sound = $InteractSound
 
-## Player class manager properties
-@export var health: float = 100
-@export var current_health: float = 100
-@export var speed: float = 4.5
-@export var jump: float = 4.5
-@export var sprint_enabled: bool = false
-@export var move_sounds_enabled: bool = false
-@export var footstep_sounds: Array[String]
-@export var sprint_sounds: Array[String]
-@export var custom_camera: bool = false
-@export var can_move: bool = true
-var gravity: float = 9.8
-
-var acceleration: float = 8
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-var mouse_sensitivity = 0.05
-
-var direction = Vector3()
-var vel = Vector3()
-var movement = Vector3()
-var gravity_vector = Vector3()
-
 var is_sprinting: bool = false
 var is_walking: bool = false
+## Enables or disables ALL motion (including camera rotate)
+var motion_enabled = true
+var mouse_sensitivity: float = 0.05
 
-func _ready():
-	$PlayerHead/PlayerRecoil/PlayerCamera.current = true
-	floor_max_angle = 1.308996
+func _ready() -> void:
 	ray.add_exception(self)
-	#watch_ray.add_exception(self)
-	if !OS.is_debug_build():
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	on_start()
 
-func on_start():
-	pass
-
-func _input(event):
-	if event is InputEventMouseMotion:
+## Mouse rotation
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion && motion_enabled:
 		rotate_y(-event.relative.x * mouse_sensitivity * 0.05)
-		player_head.rotate_x(clamp(-event.relative.y * mouse_sensitivity * 0.05, -90, 90))
+		$PlayerHead.rotate_x(-event.relative.y * mouse_sensitivity * 0.05)
 		
-		var camera_rot = player_head.rotation_degrees
-		camera_rot.x = clamp(player_head.rotation_degrees.x, -85, 85)
-		player_head.rotation_degrees = camera_rot
+		var player_rotation = $PlayerHead.rotation_degrees
+		player_rotation.x = clamp($PlayerHead.rotation_degrees.x, -85, 85)
+		$PlayerHead.rotation_degrees = player_rotation
+	
+	if Input.is_action_just_pressed("mode_kinematic"):
+		get_parent().get_node("PlayerUI").visible = !get_parent().get_node("PlayerUI").visible
 
-	direction = Vector3()
-	direction.z = -Input.get_action_strength("move_forward") + Input.get_action_strength("move_backward")
-	direction.x = -Input.get_action_strength("move_left") + Input.get_action_strength("move_right")
-	direction = direction.normalized().rotated(Vector3.UP, rotation.y)
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-func _physics_process(delta):
-	if is_on_floor():
-		gravity_vector = Vector3.ZERO
-	else:
-		gravity_vector += Vector3.DOWN * gravity * delta
-
+	# Handle jump.
 	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		gravity_vector = Vector3.UP * jump
-	if can_move:
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction && can_move && motion_enabled:
 		if Input.is_action_pressed("move_sprint"):
-			vel = vel.lerp(direction * speed * 2, acceleration * delta)
+			velocity.x = direction.x * SPEED * 2
+			velocity.z = direction.z * SPEED * 2
 		else:
-			vel = vel.lerp(direction * speed, acceleration * delta)
-		movement.z = vel.z + gravity_vector.z
-		movement.x = vel.x + gravity_vector.x
-		movement.y = gravity_vector.y
-		set_velocity(movement)
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
 	else:
-		set_velocity(Vector3.ZERO)
-	set_up_direction(Vector3.UP)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
 	move_and_slide()
 
 ## Animation-based footstep system.
@@ -91,6 +87,7 @@ func footstep_animate():
 			call("play_footstep_sound", false)
 		if is_sprinting:
 			call("play_footstep_sound", true)
+
 ## Make footstep sounds audible to all.
 func play_footstep_sound(sprinting: bool):
 	if sprinting:
@@ -99,29 +96,23 @@ func play_footstep_sound(sprinting: bool):
 	else:
 		walk_sounds.stream = load(footstep_sounds[rng.randi_range(0, footstep_sounds.size() - 1)])
 		walk_sounds.play()
+
 ## Health manager.
-func health_manage(amount: float):
-	if current_health + amount <= health:
-		current_health += amount
+func health_manage(amount: float, type_of_health: int, deplete_reason: String = ""):
+	if type_of_health >= current_health.size():
+		return
+	if current_health[type_of_health] + amount <= health[type_of_health]:
+		current_health[type_of_health] += amount
 	else:
-		current_health = health
-	if current_health <= 0:
-		get_tree().root.get_node("Game/GameOverLabel").show()
+		current_health[type_of_health] == health[type_of_health]
+	if current_health[type_of_health] <= 0:
 		set_physics_process(false)
-## Applies shader to the player
+		pass
+
+##s Applies shader to the player
 func apply_shader(res: String):
 	for node in get_node("PlayerHead/PlayerRecoil/PlayerCamera").get_children():
 		if node is MeshInstance3D:
 			node.visible = false
 	if get_node_or_null("PlayerHead/PlayerRecoil/PlayerCamera" + res) != null && !res.is_empty():
 		get_node("PlayerHead/PlayerRecoil/PlayerCamera" + res).visible = true
-## Sets players view
-func camera_manager(default_camera: bool):
-	if default_camera:
-		get_node("PlayerHead/PlayerRecoil/PlayerCamera").current = true
-		if !OS.is_debug_build():
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		custom_camera = false
-	else:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		custom_camera = true
